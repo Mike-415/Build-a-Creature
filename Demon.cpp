d16bd8fe763b1cbd08f029645986723d985458be
Mike-415 <mgonz160@mail.ccsf.edu>
2018-11-29 13:46:15 -0800

    Implemented the derived class Demon from Creature

---
+++ Demon.cpp
@@ -0,0 +1,28 @@
+//
+// Created by Michael Gonzalez on 11/29/18.
+//
+
+#include "Demon.h"
+
+namespace cs_creature{
+    Demon::Demon(){
+
+    }
+    Demon::Demon(int newStrength, int newHitpoints)
+            :Creature(newStrength, newHitpoints){
+    }
+
+    int Demon::getDamage() const{
+        // Demons can inflict damage of 50 with a 25% chance
+        int damage = Creature::getDamage();
+        if (rand() % 4 == 0) {
+            damage = damage + 50;
+            std::cout << "Demonic attack inflicts 50 additional damage points!" << std::endl;
+        }
+        return damage;
+    }
+
+    std::string Demon::getSpecies() const{
+        return "Demon";
+    }
+}
---
+++ Demon.h
@@ -0,0 +1,21 @@
+//
+// Created by Michael Gonzalez on 11/29/18.
+//
+
+#ifndef INHERITANCE_DEMON_H
+#define INHERITANCE_DEMON_H
+
+#include "Creature.h"
+namespace cs_creature{
+    class Demon: public Creature{
+    public:
+        Demon();
+        Demon(int newStrength, int newHitpoints);
+        int getDamage() const;
+        std::string getSpecies() const;
+    };
+}
+
+
+
+#endif //INHERITANCE_DEMON_H
