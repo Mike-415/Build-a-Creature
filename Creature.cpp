f41cfa56b99e7616f75defe3184bd48ddaa6a55b
Mike-415 <mgonz160@mail.ccsf.edu>
2018-11-29 12:48:06 -0800

    Modified the code according to the requirements.  The base class 'Creature' is good to go

--- Creature.cpp
+++ Creature.cpp
@@ -10,63 +10,70 @@ using namespace std;
 namespace cs_creature{
     Creature::Creature(){
         // initialize to Human, 10 strength, 10 hitpoints
-
+        strength = 10;
+        hitpoints = 10;
     }
 
-    Creature::Creature(int newTupe, int newStrength, int newHitpoints){
-
+    Creature::Creature(int newStrength, int newHitpoints){
+        strength = newStrength;
+        hitpoints = newHitpoints;
     }
 
     int Creature::getDamage() const {
         int damage;
 
-        // All Creatures inflict damage which is a random number up to their strength
+//        // All Creatures inflict damage which is a random number up to their strength
         damage = (rand() % strength) + 1;
-        cout << getSpecies() << " attacks for " << damage << " points!" << endl;
-
-        // Demons can inflict damage of 50 with a 25% chance
-        if (type == 2 || type == 1){
-            if (rand() % 4 == 0) {
-                damage = damage + 50;
-                cout << "Demonic attack inflicts 50 additional damage points!" << endl;
-            }
-        }
-
-        // Elves inflict double magical damage with a 50% chance
-        if (type == 3) {
-            if ((rand() % 2) == 0) {
-                cout << "Magical attack inflicts " << damage << " additional damage points!" << endl;
-                damage *= 2;
-            }
-        }
-
-        // Balrogs are so fast they get to attack twice
-        if (type == 2) {
-            int damage2 = (rand() % strength) + 1;
-            cout << "Balrog speed attack inflicts " << damage2 << " additional damage points!" << endl;
-            damage += damage2;
-        }
+//        cout << getSpecies() << " attacks for " << damage << " points!" << endl;
+//
+//        // Demons can inflict damage of 50 with a 25% chance
+//        if (type == 2 || type == 1){
+//            if (rand() % 4 == 0) {
+//                damage = damage + 50;
+//                cout << "Demonic attack inflicts 50 additional damage points!" << endl;
+//            }
+//        }
+//
+//        // Elves inflict double magical damage with a 50% chance
+//        if (type == 3) {
+//            if ((rand() % 2) == 0) {
+//                cout << "Magical attack inflicts " << damage << " additional damage points!" << endl;
+//                damage *= 2;
+//            }
+//        }
+//
+//        // Balrogs are so fast they get to attack twice
+//        if (type == 2) {
+//            int damage2 = (rand() % strength) + 1;
+//            cout << "Balrog speed attack inflicts " << damage2 << " additional damage points!" << endl;
+//            damage += damage2;
+//        }
 
         return damage;
     }
 
-    int Creature::getHitpoints() const {
+    int Creature::getStrength() const{
+        return strength;
+    }
 
+    int Creature::getHitpoints() const {
+        return hitpoints;
     }
     void Creature::setStrength(int newStrength) {
-
+        strength = newStrength;
     }
     void Creature::setHitpoints(int newHitpoints) {
-
+        hitpoints = newHitpoints;
     }
 
     string Creature::getSpecies() const {
-        switch (type) {
-            case 0: return "Human";
-            case 1: return "Cyberdemon";
-            case 2: return "Balrog";
-            case 3: return "Elf";
-        }
-        return "unknown";
+        return "Creature";
+//        switch (type) {
+//            case 0: return "Human";
+//            case 1: return "Cyberdemon";
+//            case 2: return "Balrog";
+//            case 3: return "Elf";
+//        }
+//        return "unknown";
     }
 }
--- Creature.h
+++ Creature.h
@@ -8,17 +8,17 @@
 namespace cs_creature{
     class Creature {
         private:
-            int type; //0 = Human, 1 = Cyberdemon, 2 = Balrog, 3 = Elf
             int strength;
             int hitpoints;
-            std::string getSpecies() const;  //Returns species type
         public:
             Creature();
-            Creature(int newTupe, int newStrength, int newHitpoints);
+            Creature(int newStrength, int newHitpoints);
             int getDamage() const;
+            int getStrength() const;
             int getHitpoints() const;
             void setStrength(int newStrength);
             void setHitpoints(int newHitpoints);
+            std::string getSpecies() const;
     };
 }
 #endif //INHERITANCE_CREATURE_H
